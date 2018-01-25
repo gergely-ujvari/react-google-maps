b0537aa9d30e832a7f1f6c3604fdf422d5edef9d
Gergely Ujvari <gergely.ujvari@icontest.hu>
2018-01-25 13:20:10 +0100

    f7

--- src/components/places/Autocomplete.jsx
+++ src/components/places/Autocomplete.jsx
@@ -9,7 +9,7 @@ import _ from "lodash"
 import React from "react"
 import ReactDOM from "react-dom"
 import PropTypes from "prop-types"
-//import * as helpers from "../../utils/AutocompleteHelper"
+import * as helpers from "../../utils/AutocompleteHelper"
 
 import {
   construct,
@@ -88,6 +88,92 @@ export class Autocomplete extends React.PureComponent {
 
   state = {
     [AUTO_COMPLETE]: null,
+  }
+
+  componentWillMount() {
+    invariant(
+      google.maps.places,
+      `Did you include "libraries=places" in the URL?`
+    )
+
+    this.handleInitializeAutoComplete()
+  }
+
+  componentDidMount() {
+    let autoComplete = this.state[AUTO_COMPLETE]
+
+    componentDidMount(this, autoComplete, eventMap)
+    this.handleMountAtControlPosition()
+  }
+
+  componentWillUpdate(nextProp) {
+    if (this.props.controlPosition !== nextProp.controlPosition) {
+      this.handleUnmountAtControlPosition()
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    componentDidUpdate(
+      this,
+      this.state[AUTO_COMPLETE],
+      eventMap,
+      updaterMap,
+      prevProps
+    )
+    if (this.props.controlPosition !== prevProps.controlPosition) {
+      this.handleMountAtControlPosition()
+    }
+  }
+
+  componentWillUnmount() {
+    componentWillUnmount(this)
+    this.handleUnmountAtControlPosition()
+    if (React.version.match(/^16/)) {
+      return
+    }
+    if (this.containerElement) {
+      ReactDOM.unmountComponentAtNode(this.containerElement)
+      this.containerElement = null
+    }
+  }
+
+  handleInitializeAutoComplete() {
+    this.containerElement = helpers.createInputElement(this.props)
+
+    /*
+     * @see https://developers.google.com/maps/documentation/javascript/3.exp/reference#Autocomplete
+     */
+    const autocomplete = new google.maps.places.Autocomplete(
+      this.containerElement,
+      this.props.options
+    )
+    construct(AutoComplete.propTypes, updaterMap, this.props, autocomplete)
+
+    this.setState({
+      [AUTO_COMPLETE]: autocomplete,
+    })
+
+    return autocomplete
+  }
+
+  handleMountAtControlPosition() {
+    if (isValidControlPosition(this.props.controlPosition)) {
+      this.mountControlIndex = helpers.mountInputElementToControlPositionOnMap(
+        this.containerElement,
+        this.props.controlPosition,
+        this.context[MAP]
+      )
+    }
+  }
+
+  handleUnmountAtControlPosition() {
+    if (isValidControlPosition(this.props.controlPosition)) {
+      helpers.unmountInputElementFromControlPositionOnMap(
+        this.mountControlIndex,
+        this.props.controlPosition,
+        this.context[MAP]
+      )
+    }
   }
 
   render() {
